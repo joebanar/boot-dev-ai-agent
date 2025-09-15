d30f5d575e63bc8e558ff294ae1d51ab295cac57
joebanar <joe.banar@gmail.com>
2025-09-15 11:17:28 -0400

    First commit for AI Agent

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+import os
+from google import genai
+from dotenv import load_dotenv
+from google.genai import types
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = args[0]
+
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if "--verbose" in args:
+        print(f"User prompt: {user_prompt}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
