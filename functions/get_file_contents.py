c154348aae9a51972a452330efbc8b97b42031ff
joebanar <joe.banar@gmail.com>
2025-09-18 08:22:59 -0400

    Update through CH3

--- functions/get_file_contents.py
+++ functions/get_file_contents.py
@@ -27,7 +27,7 @@ schema_get_file_content = types.FunctionDeclaration(
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
-            "file_type": types.Schema(
+            "file_path": types.Schema(
                 type=types.Type.STRING,
                 description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
             ),
--- main.py
+++ main.py
@@ -1,58 +1,48 @@
 import sys
 import os
-import types
 from google import genai
-from dotenv import load_dotenv
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_contents import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from dotenv import load_dotenv
+
+from prompts import system_prompt
+from call_function import call_function, available_functions
+
 
 def main():
     load_dotenv()
 
-    # HARDCODED SYSTEM PROMPT
-    system_prompt = """
-You are a helpful AI coding agent.
-
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
-
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
-
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
-    user_prompt = args[0]
-    verbose = "--verbose" in sys.argv
-    available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-        schema_get_file_content,
-        schema_run_python_file,
-        schema_write_file
-    ]
-    )
-
-    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-         config=types.GenerateContentConfig(
+        config=types.GenerateContentConfig(
             tools=[available_functions], system_instruction=system_prompt
         ),
     )
@@ -63,9 +53,20 @@ All paths you provide should be relative to the working directory. You do not ne
     if not response.function_calls:
         return response.text
 
+    function_responses = []
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
 
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
 
 
 if __name__ == "__main__":
